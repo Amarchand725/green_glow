66340456bb114b7ae630a615179bd8e1b6665494
Developer <chandamar725@gmail.com>
2022-10-11 23:31:53 +0500

    first commit

---
+++ resources/views/frontend/layouts/scripts.blade.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="{{ asset('public/frontend/js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/frontend/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/frontend/js/revslider.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/frontend/js/common.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('public/frontend/js/owl.carousel.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/frontend/js/jquery.mobile-menu.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/frontend/js/countdown.js') }}"></script>
---
+++ storage/framework/views/0b7edb40c0892d83b454cb0e20111e027f607ac8.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/bootstrap.min.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/font-awesome.min.css')); ?>" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/bootstrap.min.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/font-awesome.min.css')); ?>" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/simple-line-icons.css')); ?>" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/owl.carousel.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/owl.theme.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/jquery.bxslider.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/jquery.mobile-menu.css')); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/style.css')); ?>" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/frontend/css/revslider.css')); ?>">
+<!-- Google Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600,800,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface&amp;subset=latin-ext" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Kaushan+Script&amp;subset=latin-ext" rel="stylesheet">
+<?php /**PATH C:\xampp\htdocs\SolarEnergy\resources\views/frontend/layouts/styles.blade.php ENDPATH**/ ?>
